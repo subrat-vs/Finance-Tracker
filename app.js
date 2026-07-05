def08efa947e0cd86ea67e1f037dc2b23cd4add9
Subrat Vardhan <subrat.vs1@gmail.com>
2026-07-05 13:49:05 +0530

    Add user profile update functionality and update navigation display

--- app.js
+++ app.js
@@ -6,33 +6,30 @@ const regBtn = document.getElementById("reg-btn");
 const regLink = document.getElementById("reg-link");
 const app = document.getElementById("app");
 const logoutBtn = document.getElementById("logout")
-
 const loginUserInput = document.getElementById("login-user-input");
 const loginPassInput = document.getElementById("login-pass-input");
 const regUserInput = document.getElementById("reg-user-input");
 const regPassInput = document.getElementById("reg-pass-input");
-
 const confirmLogout = document.getElementById("confirm-logout")
 const confirmLogoutBtn = document.getElementById("confirm-logout-btn")
 const cancleLogout = document.getElementById("cancle-logout-btn")
-
 const navLinks = document.querySelectorAll(".nav-link");
-
 const menuBtn = document.getElementById("menu-btn");
 const sidebar = document.getElementById("sidebar");
 const overlay = document.getElementById("overlay");
-
+const navUser = document.getElementById("nav-user")
 const addTransactionBtn = document.getElementById("add-transaction")
 const saveTransactionBtn = document.getElementById("save-transaction")
 const transactionForm = document.getElementById("transaction-form")
 const closeForm = document.getElementById("close-form")
-
 const dashboard = document.getElementById("dashboard")
 const setting = document.getElementById("setting")
 const dashboardSec = document.getElementById("dashboard-sec")
 const settingSec = document.getElementById("setting-sec")
-
 const cashFlowChart = document.getElementById("cash-chart")
+const updateName = document.getElementById("update-name")
+const updateProfileBtn = document.getElementById("update-profile")
+
 
 const VIEWS = {
     login: {
@@ -183,4 +180,15 @@ loginBtn.addEventListener("click", () => {
 function updateChart() {
     const ctx = document.getElementById("cashFlowChart").getContext("2d")
 }
+
+{
+    navUser.textContent = localStorage.username
+    updateName.value = localStorage.username
+    updateProfileBtn.addEventListener("click", () => {
+        username = updateName.value
+        localStorage.setItem("username", username)
+        navUser.textContent = localStorage.username
+    })
+}
+
 
